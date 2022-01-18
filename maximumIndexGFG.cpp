f61bcd1df11e732567e6f6c23b5e514ad3108479
soumak09 <soumak2016@gmail.com>
2022-01-18 08:17:35 +0530

    Add files via upload

---
+++ maximumIndexGFG.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+        
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int arr[], int n) 
+    { 
+        // Your code here
+        int ans=0, i=0, j=n-1;
+        while(i<=j){
+            if(arr[i]<=arr[j]){
+                ans = max(ans,j-i);
+                i++;
+                j=n-1;
+            }
+            else{
+                j--;
+            }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+  
+/* Driver program to test above functions */
+int main() 
+{
+    int T;
+    //testcases
+    cin>>T;
+    while(T--){
+        int num;
+        //size of array
+        cin>>num;
+        int arr[num];
+        
+        //inserting elements
+        for (int i = 0; i<num; i++)
+            cin>>arr[i];
+        Solution ob;
+        
+        //calling maxIndexDiff() function
+        cout<<ob.maxIndexDiff(arr, num)<<endl;    
+        
+    }
+    return 0;
+}   // } Driver Code Ends
